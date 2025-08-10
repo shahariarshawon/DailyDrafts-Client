41345ead7625a397f111267332c74c21b87833e9
Al Shahariar Arafat Shawon <shahariararafat7739@gmail.com>
2025-08-10 22:44:43 +0600

    updated the spinner

--- src/Components/Loader/Loader.jsx
+++ src/Components/Loader/Loader.jsx
@@ -4,7 +4,8 @@ const Loader = () => {
     <div className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-br from-blue-100 via-purple-100 to-pink-100">
       <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-purple-600"></div>
       <p className="mt-4 text-purple-700 font-semibold text-lg animate-pulse">
-        Loading user info...
+        Loading user info....
+          
       </p>
     </div>
   );
