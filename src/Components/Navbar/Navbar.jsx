ca7a384bdac4f5920b6346bcd1ad0e3a4501298b
Al Shahariar Arafat Shawon <shahariararafat7739@gmail.com>
2025-08-10 22:40:11 +0600

    designed the theme toggler

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -221,7 +221,7 @@ const Navbar = () => {
 
               {/* sun icon */}
               <svg
-                className="swap-off h-10 w-10 fill-current"
+                className="swap-off h-10 w-10 fill-current text-[#4F39F6]"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
               >
@@ -230,7 +230,7 @@ const Navbar = () => {
 
               {/* moon icon */}
               <svg
-                className="swap-on h-10 w-10 fill-current"
+                className="swap-on h-10 w-10 fill-current text-[#4F39F6]"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
               >
